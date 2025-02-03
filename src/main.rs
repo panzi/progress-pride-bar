b7d83e3356e67b1cfd0b049046936a9371b64733
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2025-02-03 05:22:38 +0100

    full flag

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{fmt::Debug, io::Write, sync::{atomic::{AtomicBool, Ordering}, Arc}};
 pub mod color;
 pub mod duration;
 
-use color::{Rgb, blend, BLACK, WHITE, TRANS_PINK, TRANS_BLUE, BROWN, RED, ORANGE, BLUE, PURPLE};
+use color::{blend, Rgb, BLACK, BLUE, BROWN, GREEN, ORANGE, PURPLE, RED, TRANS_BLUE, TRANS_PINK, WHITE, YELLOW};
 use duration::Duration;
 
 fn interruptable_sleep(duration: std::time::Duration) -> bool {
@@ -44,9 +44,9 @@ fn print_line1or6(mut out: impl Write, fchars: f64, tri: &str, bar_color: Rgb, b
     let Rgb([br_r, br_g, br_b]) = BROWN;
     let Rgb([bk_r, bk_g, bk_b]) = BLACK;
 
-    if fchars > 11.0 {
-        // pink + blue + brown + black + red chunk + bar
-        let bar_fchars = fchars - 11.0;
+    if fchars > 12.5 {
+        // pink + blue + brown + black + chunk + bar
+        let bar_fchars = fchars - 12.5;
         let bar_ichars = bar_fchars as usize;
         let sub_char = ((bar_fchars - bar_ichars as f64) * 8.0) as usize;
         let Rgb([r, g, b]) = bar_color;
@@ -62,8 +62,8 @@ fn print_line1or6(mut out: impl Write, fchars: f64, tri: &str, bar_color: Rgb, b
         }
         write!(out, "\x1B[0m\x1B[K\n")?;
     } else if fchars > 10.5 {
-        // pink + blue + brown + black + red chunk
-        let Rgb([r, g, b]) = blend(background_color, bar_color, (fchars - 10.5) * 2.0);
+        // pink + blue + brown + black + chunk
+        let Rgb([r, g, b]) = blend(background_color, bar_color, (fchars - 10.5) * 0.5);
         write!(out, "\x1B[38;2;{tp_r};{tp_g};{tp_b}m\x1B[48;2;{tb_r};{tb_g};{tb_b}m{tri}\x1B[38;2;{tb_r};{tb_g};{tb_b}m\x1B[48;2;{br_r};{br_g};{br_b}m{tri}\x1B[48;2;{bk_r};{bk_g};{bk_b}m\x1B[38;2;{br_r};{br_g};{br_b}m{tri}\x1B[38;2;{bk_r};{bk_g};{bk_b}m\x1B[48;2;{r};{g};{b}m{tri}\x1B[0m\x1B[K\n")?;
     } else if fchars > 8.5 {
         // pink + blue + brown + black
@@ -97,9 +97,9 @@ fn print_line2or5(mut out: impl Write, fchars: f64, tri: &str, bar_color: Rgb, b
     let Rgb([wh_r, wh_g, wh_b]) = WHITE;
     let Rgb([bg_r, bg_g, bg_b]) = background_color;
 
-    if fchars > 12.0 {
-        // pink + blue + brown + black + red chunk + bar
-        let bar_fchars = fchars - 12.0;
+    if fchars > 13.5 {
+        // pink + blue + brown + black + chunk + bar
+        let bar_fchars = fchars - 13.5;
         let bar_ichars = bar_fchars as usize;
         let sub_char = ((bar_fchars - bar_ichars as f64) * 8.0) as usize;
         let Rgb([r, g, b]) = bar_color;
@@ -114,9 +114,9 @@ fn print_line2or5(mut out: impl Write, fchars: f64, tri: &str, bar_color: Rgb, b
             write!(out, "{}", BLOCKS[sub_char])?;
         }
         write!(out, "\x1B[0m\x1B[K\n")?;
-    } else if fchars > 11.5 {
-        // white + pink + blue + brown + black + red chunk
-        let Rgb([r, g, b]) = blend(background_color, bar_color, (fchars - 11.5) * 2.0);
+    } else if fchars > 12.0 {
+        // white + pink + blue + brown + black + chunk
+        let Rgb([r, g, b]) = blend(background_color, bar_color, (fchars - 12.0) / 1.5);
         write!(out, "\x1B[48;2;{wh_r};{wh_g};{wh_b}m\x1B[38;2;{tp_r};{tp_g};{tp_b}m{}\x1B[48;2;{tp_r};{tp_g};{tp_b}m\x1B[38;2;{tb_r};{tb_g};{tb_b}m{tri}\x1B[48;2;{tb_r};{tb_g};{tb_b}m\x1B[38;2;{br_r};{br_g};{br_b}m{tri}\x1B[38;2;{bk_r};{bk_g};{bk_b}m\x1B[48;2;{br_r};{br_g};{br_b}m{tri}\x1B[48;2;{bk_r};{bk_g};{bk_b}m\x1B[38;2;{r};{g};{b}m{tri}\x1B[0m\x1B[K\n", &tri[4..])?;
     } else if fchars > 8.5 {
         // white + pink + blue + brown + black
@@ -143,6 +143,59 @@ fn print_line2or5(mut out: impl Write, fchars: f64, tri: &str, bar_color: Rgb, b
     Ok(())
 }
 
+fn print_line3or4(mut out: impl Write, fchars: f64, tri: &str, bar_color: Rgb, background_color: Rgb) -> std::io::Result<()> {
+    let Rgb([tp_r, tp_g, tp_b]) = TRANS_PINK;
+    let Rgb([tb_r, tb_g, tb_b]) = TRANS_BLUE;
+    let Rgb([br_r, br_g, br_b]) = BROWN;
+    let Rgb([bk_r, bk_g, bk_b]) = BLACK;
+    let Rgb([wh_r, wh_g, wh_b]) = WHITE;
+
+    if fchars > 15.5 {
+        // white + pink + blue + brown + black + red chunk + bar
+        let bar_fchars = fchars - 15.5;
+        let bar_ichars = bar_fchars as usize;
+        let sub_char = ((bar_fchars - bar_ichars as f64) * 8.0) as usize;
+        let Rgb([r, g, b]) = bar_color;
+
+        write!(out, "\x1B[38;2;{wh_r};{wh_g};{wh_b}m\x1B[48;2;{tp_r};{tp_g};{tp_b}m█{tri}\x1B[38;2;{tp_r};{tp_g};{tp_b}m\x1B[48;2;{tb_r};{tb_g};{tb_b}m{tri}\x1B[38;2;{tb_r};{tb_g};{tb_b}m\x1B[48;2;{br_r};{br_g};{br_b}m{tri}\x1B[48;2;{bk_r};{bk_g};{bk_b}m\x1B[38;2;{br_r};{br_g};{br_b}m{tri}\x1B[38;2;{bk_r};{bk_g};{bk_b}m\x1B[48;2;{r};{g};{b}m{tri}\x1B[0m\x1B[38;2;{r};{g};{b}m")?;
+
+        for _ in 0..bar_ichars {
+            write!(out, "█")?;
+        }
+
+        if sub_char > 0 {
+            write!(out, "{}", BLOCKS[sub_char])?;
+        }
+        write!(out, "\x1B[0m\x1B[K\n")?;
+    } else if fchars > 13.5 {
+        // white + pink + blue + brown + black + red chunk
+        let Rgb([r, g, b]) = blend(background_color, bar_color, (fchars - 13.5) * 0.5);
+        write!(out, "\x1B[38;2;{wh_r};{wh_g};{wh_b}m\x1B[48;2;{tp_r};{tp_g};{tp_b}m█{tri}\x1B[38;2;{tp_r};{tp_g};{tp_b}m\x1B[48;2;{tb_r};{tb_g};{tb_b}m{tri}\x1B[38;2;{tb_r};{tb_g};{tb_b}m\x1B[48;2;{br_r};{br_g};{br_b}m{tri}\x1B[48;2;{bk_r};{bk_g};{bk_b}m\x1B[38;2;{br_r};{br_g};{br_b}m{tri}\x1B[38;2;{bk_r};{bk_g};{bk_b}m\x1B[48;2;{r};{g};{b}m{tri}\x1B[0m\x1B[K\n")?;
+    } else if fchars > 8.5 {
+        // white + pink + blue + brown + black
+        let Rgb([r, g, b]) = blend(background_color, BLACK, (fchars - 8.5) * 0.5);
+        write!(out, "\x1B[38;2;{wh_r};{wh_g};{wh_b}m\x1B[48;2;{tp_r};{tp_g};{tp_b}m█{tri}\x1B[38;2;{tp_r};{tp_g};{tp_b}m\x1B[48;2;{tb_r};{tb_g};{tb_b}m{tri}\x1B[38;2;{tb_r};{tb_g};{tb_b}m\x1B[48;2;{br_r};{br_g};{br_b}m{tri}\x1B[48;2;{r};{g};{b}m\x1B[38;2;{br_r};{br_g};{br_b}m{tri}\x1B[38;2;{r};{g};{b}m\x1B[49m{tri}\x1B[0m\x1B[K\n")?;
+    } else if fchars > 6.5 {
+        // white + pink + blue + brown
+        let Rgb([r, g, b]) = blend(background_color, BROWN, (fchars - 6.5) * 0.5);
+        write!(out, "\x1B[38;2;{wh_r};{wh_g};{wh_b}m\x1B[48;2;{tp_r};{tp_g};{tp_b}m█{tri}\x1B[38;2;{tp_r};{tp_g};{tp_b}m\x1B[48;2;{tb_r};{tb_g};{tb_b}m{tri}\x1B[38;2;{tb_r};{tb_g};{tb_b}m\x1B[48;2;{r};{g};{b}m{tri}\x1B[49m\x1B[38;2;{r};{g};{b}m{tri}\x1B[0m\x1B[K\n")?;
+    } else if fchars > 4.5 {
+        // white + pink + blue
+        let Rgb([r, g, b]) = blend(background_color, TRANS_BLUE, (fchars - 4.5) * 0.5);
+        write!(out, "\x1B[38;2;{wh_r};{wh_g};{wh_b}m\x1B[48;2;{tp_r};{tp_g};{tp_b}m█{tri}\x1B[38;2;{tp_r};{tp_g};{tp_b}m\x1B[48;2;{r};{g};{b}m{tri}\x1B[38;2;{r};{g};{b}m\x1B[49m{tri}\x1B[0m\x1B[K\n")?;
+    } else if fchars > 2.5 {
+        // white + pink
+        let Rgb([r, g, b]) = blend(background_color, TRANS_PINK, (fchars - 2.5) * 0.5);
+        write!(out, "\x1B[38;2;{wh_r};{wh_g};{wh_b}m\x1B[48;2;{r};{g};{b}m█{tri}\x1B[38;2;{r};{g};{b}m\x1B[49m{tri}\x1B[0m\x1B[K\n")?;
+    } else {
+        // white
+        let Rgb([r, g, b]) = blend(background_color, WHITE, fchars / 2.5);
+        write!(out, "\x1B[0m\x1B[38;2;{r};{g};{b}m█{tri}\x1B[0m\x1B[K\n")?;
+    }
+
+    Ok(())
+}
+
 pub fn print_flag(mut out: impl Write, width: usize, value: f64, background_color: Rgb) -> std::io::Result<()> {
     // trinangle widths
     // white: 2.5 chars
@@ -151,15 +204,15 @@ pub fn print_flag(mut out: impl Write, width: usize, value: f64, background_colo
     // brown: 2   chars
     // black: 2   chars
     // 
-    // then 0.5 chars at the start of the bars
+    // then 2.0 overlapping chars at the start of the full blocks
 
-    let fchars = (width as f64 + 2.5) * value;
+    let fchars = (width as f64 + 4.5) * value;
 
-    print_line1or6(&mut out, fchars, "🭏🬼", RED, background_color)?;
+    print_line1or6(&mut out, fchars, "🭏🬼", RED,    background_color)?;
     print_line2or5(&mut out, fchars, "🭢🭕", ORANGE, background_color)?;
-    // TODO: line 3
-    // TODO: line 4
-    print_line2or5(&mut out, fchars, "🭇🭄", BLUE, background_color)?;
+    print_line3or4(&mut out, fchars, "🭏🬼", YELLOW, background_color)?;
+    print_line3or4(&mut out, fchars, "🭠🭗", GREEN,  background_color)?;
+    print_line2or5(&mut out, fchars, "🭇🭄", BLUE,   background_color)?;
     print_line1or6(&mut out, fchars, "🭠🭗", PURPLE, background_color)?;
 
     Ok(())
@@ -223,7 +276,7 @@ fn main() {
             }
 
             if i != 0 {
-                let _ = write!(out, "\x1B[4A");
+                let _ = write!(out, "\x1B[6A");
             }
 
             let _ = print_flag(&mut out, width, i as f64 / steps as f64, args.background);
